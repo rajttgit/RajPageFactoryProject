3ff87ab30d905605d91d7c4752379e02e3932edf
Mohammad N Haq <myemail@myemail.com>
2018-05-11 22:34:06 -0400

    First Commit

---
+++ PageFactoryTest/src/com/page/factory/PageDriver.java
@@ -0,0 +1,54 @@
+package com.page.factory;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class PageDriver {
+WebDriver driver = null;
+	
+	@BeforeMethod
+	public void beforeMethod() {
+	System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+	driver=new ChromeDriver();
+	
+	}
+
+	
+	@Test
+	public void upsHomePageTest() throws InterruptedException{
+		driver.get("https://www.ups.com/us/en/Home.page");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		UpsFirstPage up1 = new UpsFirstPage(driver);
+		up1.tracking().click();
+		Thread.sleep(1000);
+		up1.shipping().click();
+		Thread.sleep(1000);
+		up1.services().click();
+		
+		
+	}	
+	
+	
+	
+	@AfterMethod
+	public void afterMethod() throws InterruptedException {
+		Thread.sleep(2000);
+		driver.quit();
+	}
+
+	
+	
+	
+	
+}
---
+++ PageFactoryTest/src/com/page/factory/UpsFirstPage.java
@@ -0,0 +1,41 @@
+package com.page.factory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class UpsFirstPage {
+
+	WebDriver driver;
+	
+	public UpsFirstPage(WebDriver d){
+		this.driver=d;
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	@FindBy(xpath="//*[@id='ups-menuLinks0']")
+	WebElement tracking;
+	
+	@FindBy(xpath="//*[@id='ups-menuLinks1']")
+	WebElement shipping;
+	
+	@FindBy(xpath="//*[@id='ups-menuLinks2']")
+	WebElement services;
+	
+	
+	public WebElement tracking(){
+		return tracking;
+	}
+	public WebElement shipping(){
+		return shipping;
+	}
+	public WebElement services(){
+		return services;
+	}
+	
+	
+	
+	
+}
